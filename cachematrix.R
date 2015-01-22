7ae5a8cc5246269320766eebc12553d950b3d074
chenxiang12 <chenxneu@163.com>
2015-01-22 11:36:48 +0800

    Complete the requirment in programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing the inverse of a Square Matrix, if the inverse of the Square Matrix was onced computed,
+## keep it in cache for next fast retrive.
 
-## Write a short comment describing this function
+## Create a matrix with 4 operations as follows
+## setMt/getMt: Set/get the square matrix
+## setRMt/getRMt: Set/get the inverse of the specified suqare matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## Inverse of the square matrix 
+    r <- NULL
+    setMt <- function(y){
+        x <<- y
+        r <<- NULL
+    }
+    getMt <- function() x
+    setRMt <- function(y) r <<- y
+    getRMt <- function() r
+    ## return a list contains all the 4 opetations
+    list(setMt = setMt, getMt = getMt, setRMt = setRMt, getRMt = getRMt)
 
 }
 
 
-## Write a short comment describing this function
+## Calculate the inverse of the square matrix if onced not computed
+## and then return the inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+            
+    r <- x$getRMt()
+    ## Check whether the inverse matrix has already cached
+    if(!is.null(r)){
+        message("Get cache inverse matrix")
+        return (r)
+    }
+    m <- x$getMt()
+    r <- solve(m, ...)
+    ## Save the computed result in cache
+    x$setRMt(r)
+    r
 }
